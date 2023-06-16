3c61b0a0e8bc53e46bdbd169ac5618fa776b76d0
Jeongmin <srm0609@naver.com>
2023-06-16 10:19:34 +0900

    Create [postingID].tsx

---
+++ src/pages/detail/[postingID].tsx
@@ -0,0 +1,13 @@
+import Header from 'components/common/molecules/Header'
+import Detail from 'components/Detail/organisms/Detail'
+
+const DetailPage = () => {
+  return (
+    <>
+      <Header />
+      <Detail />
+    </>
+  )
+}
+
+export default DetailPage
