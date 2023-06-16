173a5e5de82ad319edba91db976515cf30fce4c1
Jeongmin <srm0609@naver.com>
2023-06-16 10:22:50 +0900

    Create: SuggestPost

---
+++ src/components/Detail/molecules/SuggestPost/index.tsx
@@ -0,0 +1,13 @@
+import Announcement from 'components/Home/molecules/Announcement'
+import * as S from './style'
+
+const SuggestPost = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>여기는 어떤가요?</S.Title>
+      <Announcement />
+    </S.Wrapper>
+  )
+}
+
+export default SuggestPost
---
+++ src/components/Detail/molecules/SuggestPost/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  padding: 50px 0;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.div`
+  font-size: 22px;
+  font-weight: 700;
+`
