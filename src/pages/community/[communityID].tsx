a14b2d4ed029b6eb526a6a384eed25ea29b7818b
Jeongmin <srm0609@naver.com>
2023-06-18 04:19:04 +0900

    Create: CommunityDetailPage

---
+++ src/pages/community/[communityID].tsx
@@ -0,0 +1,13 @@
+import CommunityDetail from 'components/Community/organisms/CommunityDetail'
+import Header from 'components/common/molecules/Header'
+
+const CommunityDetailPage = () => {
+  return (
+    <>
+      <Header />
+      <CommunityDetail />
+    </>
+  )
+}
+
+export default CommunityDetailPage
