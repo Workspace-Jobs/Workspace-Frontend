a7cfb0a2872e4debcf4e4cda9b2a022c76b5f27b
Jeongmin <srm0609@naver.com>
2023-06-18 07:40:10 +0900

    Create: MyPage

---
+++ src/pages/my/index.tsx
@@ -0,0 +1,13 @@
+import My from 'components/My/organisms/My'
+import Header from 'components/common/molecules/Header'
+
+const MyPage = () => {
+  return (
+    <>
+      <Header />
+      <My />
+    </>
+  )
+}
+
+export default MyPage
