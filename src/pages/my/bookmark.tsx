691c1f96e5a0a636189311fbfbda97ad516051cf
Jeongmin <srm0609@naver.com>
2023-06-18 07:44:16 +0900

    Create: BookmarkPage

---
+++ src/pages/my/bookmark.tsx
@@ -0,0 +1,13 @@
+import Bookmark from 'components/My/organisms/Bookmark'
+import Header from 'components/common/molecules/Header'
+
+const BookmarkPage = () => {
+  return (
+    <>
+      <Header />
+      <Bookmark />
+    </>
+  )
+}
+
+export default BookmarkPage
