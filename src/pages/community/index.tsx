2d0fda6e6d42fd4870bb27cab7d0bde43b5e38e5
Jeongmin <srm0609@naver.com>
2023-06-17 23:40:26 +0900

    Create: CommunityPage

---
+++ src/pages/community/index.tsx
@@ -0,0 +1,13 @@
+import Community from 'components/Community/organisms/Community'
+import Header from 'components/common/molecules/Header'
+
+const CommunityPage = () => {
+  return (
+    <>
+      <Header />
+      <Community />
+    </>
+  )
+}
+
+export default CommunityPage
