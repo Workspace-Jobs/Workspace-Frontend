4c9f15c04a7dc6b68642bac43dc05119c650ca7f
Jeongmin <srm0609@naver.com>
2023-06-18 08:09:36 +0900

    Create: ResumeItem

---
+++ src/components/Resume/atoms/ResumeItem/intex.tsx
@@ -0,0 +1,10 @@
+import * as S from './style'
+
+const ResumeItem = () => {
+  return (
+    <S.Wrapper>
+      <h3>이력서 이름</h3>
+    </S.Wrapper>
+  )
+}
+export default ResumeItem
---
+++ src/components/Resume/atoms/ResumeItem/style.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import { Palette } from 'styles/globals'
+
+export const Wrapper = styled.div`
+  width: 284px;
+  height: 200px;
+  padding: 24px 30px;
+  background-color: ${Palette.BACKGROUND_BG};
+  border-radius: 16px;
+`
