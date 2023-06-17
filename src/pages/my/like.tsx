129a5bcbb918ed8d7da1e98e0a79a8577a2f7c9c
Jeongmin <srm0609@naver.com>
2023-06-18 07:44:34 +0900

    Create: LikePage

---
+++ src/pages/my/like.tsx
@@ -0,0 +1,13 @@
+import Header from 'components/common/molecules/Header'
+import Like from 'components/My/organisms/Like'
+
+const LikePage = () => {
+  return (
+    <>
+      <Header />
+      <Like />
+    </>
+  )
+}
+
+export default LikePage
