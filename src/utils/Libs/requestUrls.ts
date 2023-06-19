5b250b0a7940090a286486bc4c9fb7921aef1ff0
Jeongmin <srm0609@naver.com>
2023-06-20 01:14:18 +0900

    Update requestUrls.ts

--- src/utils/Libs/requestUrls.ts
+++ src/utils/Libs/requestUrls.ts
@@ -5,3 +5,50 @@ export const MemberController = {
   changeName: '/user/name/',
   user: '/user/main',
 }
+
+export const ResumeController = {
+  resume: '/user/resume/',
+  getResume(id: string) {
+    return `/user/resume/${id}`
+  },
+}
+
+export const CommunityController = {
+  postCommunity: '/NB/',
+  community(id: string) {
+    return `/NB/${id}`
+  },
+  getCommunityList: `/NB/list/`,
+  tagCommunity: `/NB/list/tag/`,
+  likeCommunity(id: string) {
+    return `/NB/good/${id}`
+  },
+}
+
+export const EmplymentController = {
+  employment: '/EM/',
+  employmentJob: '/EM/job/',
+  employmentSearch: '/EM/search/',
+  employmentDetail(id: string) {
+    return `/EM/${id}`
+  },
+  employmentBookmark(id: number) {
+    return `/EM/mark/${id}`
+  },
+  employmentApply(id: string) {
+    return `/EM/su/${id}`
+  },
+}
+
+export const MyController = {
+  profile: '/NP/user/',
+  bookmarkList: '/NP/mark',
+  likeList: '/NP/good/',
+  myList: '/NP/NB/',
+  changeImg: '/NP/profile/',
+  myStatus: '/NP/state/',
+  myStatusList: '/NP/state/list/',
+  myApply(id: string) {
+    return `/NP/support/${id}`
+  },
+}
