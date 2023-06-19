1dbd5bceb31835166e46fc4e441f3835f20cb1f3
Jeongmin <srm0609@naver.com>
2023-06-20 01:16:48 +0900

    Update Home.tsx

--- src/components/Home/organisms/Home.tsx
+++ src/components/Home/organisms/Home.tsx
@@ -1,12 +1,24 @@
 import { CommonTemplate } from 'components/common/templates/CommonTemplate/style'
 import Announcement from '../molecules/Announcement'
 import NewAnnouncement from '../molecules/NewAnnouncement'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { pageNum } from 'Atoms/recoilAtom'
+import { getEmployment } from 'api/employment'
 
 const Home = () => {
+  const [page, setPage] = useRecoilState(pageNum)
+  const [listData, setListData] = useState()
+  useEffect(() => {
+    getEmployment(page).then((res) => {
+      setListData(res?.data)
+    })
+  }, [])
+
   return (
     <CommonTemplate>
-      <NewAnnouncement />
-      <Announcement />
+      <NewAnnouncement list={listData} />
+      <Announcement list={listData} />
     </CommonTemplate>
   )
 }
