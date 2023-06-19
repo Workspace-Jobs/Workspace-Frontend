0f901d084964c24c59a24807504b62d0d9bbfb5f
Jeongmin <srm0609@naver.com>
2023-06-20 01:26:54 +0900

    Fix: ci error fix
    주석처리

--- src/components/Employment/organisms/Employment.tsx
+++ src/components/Employment/organisms/Employment.tsx
@@ -1,12 +1,25 @@
 import Announcement from 'components/Home/molecules/Announcement'
 import { CommonTemplate } from 'components/common/templates/CommonTemplate/style'
 import Filter from '../molecules/Filter'
+import { getEmployment } from 'api/employment'
+import { useRecoilState } from 'recoil'
+import { pageNum } from 'Atoms/recoilAtom'
+import { useEffect, useState } from 'react'
 
 const Employment = () => {
+  const [page, setPage] = useRecoilState(pageNum)
+  const [listData, setListData] = useState()
+  useEffect(() => {
+    getEmployment(page).then((res) => {
+      console.log(res?.data)
+      setListData(res?.data)
+    })
+  }, [])
+
   return (
     <CommonTemplate>
       <Filter />
-      <Announcement />
+      <Announcement list={listData} />
     </CommonTemplate>
   )
 }
