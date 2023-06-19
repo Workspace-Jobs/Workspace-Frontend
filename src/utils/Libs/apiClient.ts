fb6e1d820aa791ea67db98ee7769fb960d13ee03
Jeongmin <srm0609@naver.com>
2023-06-19 23:45:07 +0900

    Update apiClient.ts

--- src/utils/Libs/apiClient.ts
+++ src/utils/Libs/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
-import BASE_HEADER from '../Config/Config.json'
+import { getRefresh } from './getRefresh'
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  headers: BASE_HEADER,
 })
+
+apiClient.interceptors.request.use(getRefresh)
