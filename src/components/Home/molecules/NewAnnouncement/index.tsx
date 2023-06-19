190f1697fdffdae5f7ee7fb714d52938a4b72591
Jeongmin <srm0609@naver.com>
2023-06-20 01:17:06 +0900

    Update: NewAnnouncement

--- src/components/Home/molecules/NewAnnouncement/index.tsx
+++ src/components/Home/molecules/NewAnnouncement/index.tsx
@@ -1,17 +1,24 @@
 import * as S from './style'
 import NewListItem from 'components/Home/atoms/NewPostItem'
 import Profile from 'components/Home/atoms/Profile'
+import { EmploylistType } from 'types/components/employment'
 
-const NewAnnouncement = () => {
+const NewAnnouncement = ({ list }: EmploylistType) => {
   return (
     <S.Section>
       <S.Wrapper>
         <S.Title>최신 공고</S.Title>
         <S.ListWrapper>
-          <NewListItem />
-          <NewListItem />
-          <NewListItem />
-          <NewListItem />
+          {list?.slice(0, 4).map((e) => (
+            <NewListItem
+              key={e.id}
+              id={e.id}
+              title={e.title}
+              img1={e.img1}
+              user={e.user}
+              date={e.date}
+            />
+          ))}
           <Profile />
         </S.ListWrapper>
       </S.Wrapper>
