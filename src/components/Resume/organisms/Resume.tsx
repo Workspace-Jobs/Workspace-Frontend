20f92feae03fa3bee727f4102d629f3d8a6c796d
Jeongmin <srm0609@naver.com>
2023-06-20 21:11:55 +0900

    Update: resume
    list 불러오기

--- src/components/Resume/organisms/Resume.tsx
+++ src/components/Resume/organisms/Resume.tsx
@@ -2,13 +2,23 @@ import CommonTitle from 'components/common/atoms/CommonTitle'
 import { CommonTemplate } from 'components/common/templates/CommonTemplate/style'
 import { ResumeTemplate } from '../style'
 import ResumeList from '../molecules/ResumeList/intex'
+import { useEffect, useState } from 'react'
+import { getResume } from 'api/resume'
+import { ResumeListProps } from 'types/components/Resume'
 
 const Resume = () => {
+  const [data, setData] = useState<ResumeListProps[]>()
+
+  useEffect(() => {
+    getResume().then((res) => {
+      setData(res?.data)
+    })
+  }, [])
   return (
     <CommonTemplate>
       <CommonTitle title="내 이력서" />
       <ResumeTemplate>
-        <ResumeList />
+        <ResumeList list={data} />
       </ResumeTemplate>
     </CommonTemplate>
   )
