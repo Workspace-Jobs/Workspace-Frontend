fbec1ef311717e6719397efbe2fbccffe0d0b777
Jeongmin <srm0609@naver.com>
2023-06-20 21:12:09 +0900

    Update: ResumeList

--- src/components/Resume/molecules/ResumeList/intex.tsx
+++ src/components/Resume/molecules/ResumeList/intex.tsx
@@ -1,16 +1,14 @@
-import * as S from './style'
 import * as SVG from 'assets/svg'
 import ResumeItem from 'components/Resume/atoms/ResumeItem/intex'
+import { ResumelistType } from 'types/components/Resume'
+import * as S from './style'
 
-const ResumeList = () => {
+const ResumeList = ({ list }: ResumelistType) => {
   return (
     <S.Wrapper>
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
+      {list?.map((e, ind) => (
+        <ResumeItem key={ind} id={e.id} user={e.user} name={e.name} />
+      ))}
       <S.AddResume>
         <SVG.PlusIcon />
         <p>새로운 이력서를 추가해보세요!</p>
