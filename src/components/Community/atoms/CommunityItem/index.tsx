8e336c062abcdde923bf75a8774681e5ba0d28d4
Jeongmin <srm0609@naver.com>
2023-06-20 20:47:07 +0900

    Update: Item

--- src/components/Community/atoms/CommunityItem/index.tsx
+++ src/components/Community/atoms/CommunityItem/index.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link'
 import * as S from './style'
+import { CommunityListProps } from 'types/components/Community'
+import dateFillter from 'utils/dateFillter'
 
-const CommunityItem = () => {
+const CommunityItem = ({
+  id,
+  user,
+  title,
+  created_date,
+}: CommunityListProps) => {
   return (
-    <Link href="/community/1" passHref>
+    <Link href={`/community/${id}`} passHref>
       <S.Wrapper>
-        <S.SubText size={'12px'}>123</S.SubText>
-        <S.SubText size={'16px'}>TITLE</S.SubText>
-        <S.SubText size={'12px'}>2023.03.03</S.SubText>
+        <S.SubText size={'12px'}>{id}</S.SubText>
+        <S.SubText size={'16px'}>{title}</S.SubText>
+        <S.SubText size={'12px'}>{dateFillter(created_date)}</S.SubText>
       </S.Wrapper>
     </Link>
   )
