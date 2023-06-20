b8be2159098332f5929809a485b9317bff5c4c8e
Jeongmin <srm0609@naver.com>
2023-06-20 19:45:20 +0900

    Update: Employment
    필터검색

--- src/components/Employment/organisms/Employment.tsx
+++ src/components/Employment/organisms/Employment.tsx
@@ -1,20 +1,25 @@
 import Announcement from 'components/Home/molecules/Announcement'
 import { CommonTemplate } from 'components/common/templates/CommonTemplate/style'
 import Filter from '../molecules/Filter'
-import { getEmployment } from 'api/employment'
-import { useRecoilState } from 'recoil'
-import { pageNum } from 'Atoms/recoilAtom'
+import { getEmployment, getJobEmployment } from 'api/employment'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { filterMenu, pageNum } from 'Atoms/recoilAtom'
 import { useEffect, useState } from 'react'
 
 const Employment = () => {
   const [page, setPage] = useRecoilState(pageNum)
   const [listData, setListData] = useState()
+  const menu = useRecoilValue(filterMenu)
+
   useEffect(() => {
-    getEmployment(page).then((res) => {
-      console.log(res?.data)
-      setListData(res?.data)
-    })
-  }, [])
+    menu === '개발 전체'
+      ? getEmployment(page).then((res) => {
+          setListData(res?.data)
+        })
+      : getJobEmployment(page, menu).then((res) => {
+          setListData(res?.data)
+        })
+  }, [menu])
 
   return (
     <CommonTemplate>
