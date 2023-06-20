4923c555f25cae03696a6b36b7b35ff23fca85a4
Jeongmin <srm0609@naver.com>
2023-06-20 20:32:10 +0900

    Fix: Ci error

--- src/components/Employment/atoms/FilterMenuModal/index.tsx
+++ src/components/Employment/atoms/FilterMenuModal/index.tsx
@@ -1,8 +1,13 @@
 import { PostFilterData } from 'assets/data/FilterData'
 import FilterItem from 'components/common/atoms/FilterItem'
 import { ModalOverayWrapper } from 'components/common/atoms/ModalOverayWrapper/style'
-import { EmployFilterType } from 'types/components/Employment'
 import * as S from './style'
+import { ModalType } from 'types/common/Modal'
+
+interface EmployFilterType extends ModalType {
+  menu: string
+  onClick: (filterMenu: string) => void
+}
 
 const FilterMenuModal = ({
   menu,
