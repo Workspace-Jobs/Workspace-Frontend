48a16043e542077bdc201d98514664f41b7e9399
Jeongmin <srm0609@naver.com>
2023-06-20 19:56:44 +0900

    Create: My types

---
+++ src/types/components/My.ts
@@ -0,0 +1,35 @@
+export interface StatusListProps {
+  id: number
+  employment: {
+    id: number
+    title: string
+    user: string
+  }
+  date: string
+}
+
+export interface BookmarkListProps {
+  id: number
+  employment: {
+    id: number
+    title: string
+    img1: string
+    user: {
+      id: number
+      username: string
+      location: string
+    }
+    date: string
+  }
+}
+
+export interface LikeListProps {
+  id: number
+  nb: {
+    id: number
+    user: number
+    title: string
+    good_cnt: number
+    created_date: string
+  }
+}
