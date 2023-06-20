0f766bb991a47f14020182c0151d0b41f873b6a3
Jeongmin <srm0609@naver.com>
2023-06-20 20:57:31 +0900

    Fix: ci error

---
+++ src/types/components/Employment.ts
@@ -0,0 +1,22 @@
+import { ModalType } from 'types/common/Modal'
+
+export interface EmployFilterType extends ModalType {
+  menu: string
+  onClick: (filterMenu: string) => void
+}
+
+export interface EmployListProps {
+  id: number
+  title: string
+  img1: string
+  user: {
+    id: number
+    username: string
+    location: string
+  }
+  date: string
+}
+
+export interface EmploylistType {
+  list: EmployListProps[] | undefined
+}
