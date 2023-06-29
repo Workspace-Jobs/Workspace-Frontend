088591663eca47de369f8b80edee759b23375367
Jeongmin <srm0609@naver.com>
2023-06-29 09:56:07 +0900

    Add: comment

--- src/components/Community/atoms/CommentInput/index.tsx
+++ src/components/Community/atoms/CommentInput/index.tsx
@@ -1,11 +1,28 @@
+import { useRecoilValue } from 'recoil'
 import * as S from './style'
+import { userName } from 'Atoms/recoilAtom'
+import { useState } from 'react'
+import { postComment } from 'api/community'
 
-const CommentInput = () => {
+const CommentInput = ({ id }: { id: number }) => {
+  const name = useRecoilValue(userName)
+  const [content, setContent] = useState<string | undefined>()
+
+  const onSubmint = () => {
+    content && postComment(id, content)
+    setContent('')
+    location.reload()
+  }
+
   return (
     <S.Wrapper>
-      <p>myname</p>
-      <textarea placeholder="댓글 작성하기" />
-      <S.Button>등록하기</S.Button>
+      <p>{name}</p>
+      <textarea
+        placeholder="댓글 작성하기"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <S.Button onClick={() => onSubmint()}>등록하기</S.Button>
     </S.Wrapper>
   )
 }
--- src/components/Community/atoms/CommentItem/index.tsx
+++ src/components/Community/atoms/CommentItem/index.tsx
@@ -1,13 +1,15 @@
+import { CommentListProps } from 'types/components/Community'
 import * as S from './style'
+import dateFillter from 'utils/dateFillter'
 
-const CommentItem = () => {
+const CommentItem = ({ id, user, nb, date, centent }: CommentListProps) => {
   return (
     <S.Wrapper>
       <S.Header>
-        <p>프로필</p>
-        <p>2023.03.03</p>
+        <p>{user}</p>
+        <p>{dateFillter(date)}</p>
       </S.Header>
-      <S.Content>content</S.Content>
+      <S.Content>{centent}</S.Content>
     </S.Wrapper>
   )
 }
